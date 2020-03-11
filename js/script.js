f371094212c75827c9667853470bf0cac2718221
Soham <sohampatelk@yahoo.com>
2020-03-11 11:23:04 -0600

    const and let

--- js/script.js
+++ js/script.js
@@ -48,3 +48,44 @@ console.log(FromArray);
  */
 var filtered = myArray.filter(function(element){return element>4; });
 console.log(filtered); //all mathches returned
+
+/**
+ * .reduce() method
+ * we can add on to a value, returns only a single.
+ */
+var reduceResult = myArray.reduce(function(accumulator,currentVal){
+    return accumulator + currentVal; //because it is string ,they are getting concatinated.
+});
+console.log(reduceResult);
+
+console.log([1,2,3,4,5].reduce(function (a,v){
+    return a+v;
+})); //beaucse we have a all numbers so it will be added mathematically.
+
+console.log([1, 2, 3, 4, 5, "sd", 5, 6, "asd", "asfaf"].reduce(function (a, v) {
+    return a + v;
+})); //it adds stars num methematically untill 1 to 6 means 15 and when it touch string it concatinates with number between 5,6 as well concatinates with string
+
+/**
+ * Let and Const
+ */
+var x=3; // standard variable declaration//it is function scope
+let y=7; // Let is block scope
+const z=35; //Cont is block scoped, but reassignment is not allowed.
+
+//z=16; not allowd!! it is const . which does not allowed to reassign
+console.log(z);
+
+y=13; //allowed var and let allows re assignment.
+console.log(y);
+
+for(var myVar = 0; myVar<10; myVar++){
+    console.log("For loop iteration");
+}
+console.log("myVar :"+myVar); // var is global variable,
+
+for (let myLetVar = 0; myLetVar < 10; myLetVar++) {
+    console.log("For loop iteration");
+    console.log("myVar :" + myLetVar);
+}
+//console.log("myVar :" + myLetVar); // it is not working here outside. it is outof scope just workes in function.
